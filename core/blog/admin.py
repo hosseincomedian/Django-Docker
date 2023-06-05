1dbffc66ba6075dc4994e7255f338acd6d4586a0
Hossein Salimi <46236489+hosseincomedian@users.noreply.github.com>
2023-06-05 12:43:47 +0330

    CategoryAdmin

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Category
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    pass    
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
     pass    
