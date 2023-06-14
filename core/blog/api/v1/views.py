307f28a7fd007e3086258a990b383bae4398dbe8
Hossein Salimi <46236489+hosseincomedian@users.noreply.github.com>
2023-06-15 00:57:21 +0330

    PostSerializer and postList and postDetail

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,10 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import PostSerializer
+from blog.models import Post    
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 @api_view()
 def postList(request):
-    return Response("ok")
+    posts = Post.objects.filter(status=True)
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
 
 @api_view()
 def postDetail(request, pk):
-    return Response(pk)
+    post = get_object_or_404(Post, pk=pk, status=True)
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
