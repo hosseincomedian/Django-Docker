e24887654d0426b9871a9766018fbe714451e184
Hossein Salimi <46236489+hosseincomedian@users.noreply.github.com>
2023-06-17 18:10:54 +0330

    edit serializers & create new permission & add djangofilters

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -9,12 +9,17 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework import viewsets
-
+from .permissions import PostAuthorPermission
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter 
 
 class PostModelViewSet(viewsets.ModelViewSet):
-        permission_classes = (IsAuthenticatedOrReadOnly,)
+        permission_classes = (IsAuthenticatedOrReadOnly, PostAuthorPermission)
         queryset = Post.objects.filter(status=True)
         serializer_class = PostSerializer   
+        filter_backends = [DjangoFilterBackend, SearchFilter]
+        filterset_fields = ('author', 'category', 'status',)
+        search_fields = ['title', 'content']
         
         
 class CategoryModelViewSet(viewsets.ModelViewSet):
