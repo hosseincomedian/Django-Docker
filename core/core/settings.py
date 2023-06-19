308da6cebd60af709ffa204355be90dc992b5c0a
Hossein Salimi <46236489+hosseincomedian@users.noreply.github.com>
2023-06-19 09:51:15 +0330

    drf_yasg

--- core/core/settings.py
+++ core/core/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #
+    'drf_yasg',
     'rest_framework',
     'django_filters',
+    
     'accounts',
     'blog',
 ]
