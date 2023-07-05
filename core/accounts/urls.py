ca8a24631bb32500c7fdbfd5c30cf4d493b940bb
Hossein Salimi <46236489+hosseincomedian@users.noreply.github.com>
2023-07-05 17:30:34 +0330

    django caches

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import send_email
+from .views import send_email, test
 app_name = "accounts"
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
-    path("test/",send_email),
+    path("send-email/",send_email),
+    path("test/",test),
     path("api/v1/", include("accounts.api.v1.urls")),
     path("api/v2/", include("djoser.urls")),
     path("api/v2/", include("djoser.urls.jwt")),
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,6 +1,12 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 import time
 from .tasks import sendEmail
+import requests
+
 def send_email(request, *args, **kwargs):
     sendEmail.delay()
     return HttpResponse("ok")
+
+def test(request, *args, **kwargs):
+    response = requests.get("https://e3a7ef5a-1f3a-4b12-8490-71bae8aff6bd.mock.pstmn.io/test/delay/5")
+    return JsonResponse(response.json())
