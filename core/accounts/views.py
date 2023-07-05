001a70e94ec30e24f17c8176056b6c2e556d77e4
Hossein Salimi <46236489+hosseincomedian@users.noreply.github.com>
2023-07-06 00:05:59 +0330

    django cache

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse, JsonResponse
 import time
-from .tasks import sendEmail
 import requests
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
+from .tasks import sendEmail
 
 def send_email(request, *args, **kwargs):
     sendEmail.delay()
     return HttpResponse("ok")
 
+@cache_page(60)
 def test(request, *args, **kwargs):
     response = requests.get("https://e3a7ef5a-1f3a-4b12-8490-71bae8aff6bd.mock.pstmn.io/test/delay/5")
     return JsonResponse(response.json())
--- core/core/settings.py
+++ core/core/settings.py
@@ -180,3 +180,13 @@ CELERY_BROKER_URL = 'redis://redis:6379/1'
 #         'schedule': 5
 #     }
 # }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
